3b3d062f202c38970ea793c63d9178036c7e2dab
Felix Kubli <felix.kubli@renuo.ch>
2016-09-26 09:58:45 +0200

    add service Time Today

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,19 @@
 import {Component} from '@angular/core';
+import {TimeToday} from "../shared";
 
 @Component({
   selector: 'my-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  providers: [TimeToday]
 })
 
 export class HomeComponent {
+  goal: number;
+  reached: number;
 
-  constructor() {
-    // Do stuff
+  constructor(private time: TimeToday) {
+    this.goal = time.goal;
+    this.reached = time.reached;
   }
 }
---
+++ src/app/shared/time_today.service.ts
@@ -0,0 +1,7 @@
+import {Injectable} from '@angular/core';
+
+@Injectable()
+export class TimeToday {
+  goal: number = 8.4;
+  reached: number = 4.2;
+}
