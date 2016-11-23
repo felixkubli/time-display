d7359a1f5e41f7ca5935928f39fa9d5374b76b4d
Felix Kubli <felix.kubli@renuo.ch>
2016-11-23 17:47:15 +0100

    extract reusable code

---
+++ src/app/utils/svg_calculator.ts
@@ -0,0 +1,29 @@
+
+export class SvgCalculator {
+  hoursToAngles(value: number, max: number) {
+    return _.round(value * 360 / max);
+  }
+
+  polarToCartesian(centerX, centerY, radius, angleInDegrees) {
+    let angleInRadians = (angleInDegrees - 90) * Math.PI / 180.0;
+
+    return {
+      x: centerX + (radius * Math.cos(angleInRadians)),
+      y: centerY + (radius * Math.sin(angleInRadians))
+    };
+  }
+
+  describeArc(x, y, radius, startAngle, endAngle) {
+    let start = this.polarToCartesian(x, y, radius, endAngle);
+    let end = this.polarToCartesian(x, y, radius, startAngle);
+
+    let largeArcFlag = endAngle - startAngle <= 180 ? '0' : '1';
+
+    let d = [
+      'M', start.x, start.y,
+      'A', radius, radius, 0, largeArcFlag, 0, end.x, end.y
+    ].join(' ');
+
+    return d;
+  }
+}
--- src/app/week/week.component.ts
+++ src/app/week/week.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import * as _ from 'lodash';
 import { WeekService } from './week.service';
 import { Week } from './week.model';
+import { SvgCalculator } from '../utils/svg_calculator'
 
 @Component({
   selector: 'my-week',
@@ -22,11 +23,13 @@ export class WeekComponent implements OnInit {
   total_diff: number;
   subscription;
   week: Week = new Week();
+  svg_calc: SvgCalculator;
 
   progress_type: string = 'primary';
 
   constructor(private weekService: WeekService) {
     this.goal = localStorage.getItem('goal_today');
+    this.svg_calc = new SvgCalculator();
   }
 
   ngOnInit() {
@@ -75,34 +78,5 @@ export class WeekComponent implements OnInit {
     } else {
       return 'difference negative';
     }
-  }
-
-  /* svg-functions */
-
-  hoursToAngles(value: number, max: number) {
-    return _.round(value * 360 / max);
-  }
-
-  polarToCartesian(centerX, centerY, radius, angleInDegrees) {
-    let angleInRadians = (angleInDegrees - 90) * Math.PI / 180.0;
-
-    return {
-      x: centerX + (radius * Math.cos(angleInRadians)),
-      y: centerY + (radius * Math.sin(angleInRadians))
-    };
-  }
-
-  describeArc(x, y, radius, startAngle, endAngle) {
-    let start = this.polarToCartesian(x, y, radius, endAngle);
-    let end = this.polarToCartesian(x, y, radius, startAngle);
-
-    let largeArcFlag = endAngle - startAngle <= 180 ? '0' : '1';
-
-    let d = [
-      'M', start.x, start.y,
-      'A', radius, radius, 0, largeArcFlag, 0, end.x, end.y
-    ].join(' ');
-
-    return d;
   }
 }
