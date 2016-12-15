46791848a1fa5128184d19a011425b82197a753c
Felix Kubli <felix.kubli@renuo.ch>
2016-12-15 09:50:42 +0100

    add test for home component

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,19 +1,41 @@
 // This shows a different way of testing a component, check about for a simpler one
-import { Component } from '@angular/core';
 
-import { TestBed } from '@angular/core/testing';
+import { TestBed, async, inject } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
 import { TimeTodayComponent } from '../time_today/time_today.component';
+import { Ng2BootstrapModule } from 'ng2-bootstrap/ng2-bootstrap';
+import { DateTodayPipe } from '../utils/date_today.pipe';
+import { NumberDiffPipe } from '../utils/number_diff.pipe';
+import { TimeTodayService } from '../time_today/time_today.service';
+import { HttpModule } from '@angular/http';
 
 describe('Home Component', () => {
-  const html = '<my-home></my-home>';
 
   beforeEach(() => {
-    TestBed.configureTestingModule({declarations: [HomeComponent, TimeTodayComponent]});
+    TestBed.configureTestingModule({
+      imports: [
+        Ng2BootstrapModule,
+        HttpModule
+      ],
+      providers: [
+        TimeTodayService,
+        HomeComponent,
+        TimeTodayComponent,
+        DateTodayPipe,
+        NumberDiffPipe
+      ]
+    });
+  });
+
+  let component: HomeComponent;
+
+  beforeEach(async(inject([HomeComponent], (_component: HomeComponent) => {
+    component = _component;
+  })));
+
+  it('input date should have the right format', () => {
+    expect(component['input_date']).toMatch(/[0-9]+-[0-9]+-[0-9]+/);
   });
 
 });
-
-@Component({selector: 'my-test', template: ''})
-class TestComponent { }
